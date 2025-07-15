6f8793151d0645efb2f7fb2e71fa95d4dbdd137e
stvdn <stevendanny2000@gmail.com>
2025-07-14 20:34:15 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/react_portfolio/",
   plugins: [react(), tailwindcss()],
 });
